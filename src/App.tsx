ef768894a81d1dd51eec6ec020c6b6e46a830f63
TransonQ <quanscheng@qq.com>
2025-08-11 18:57:25 +0800

    Add CasbinDemo component and permissions configuration
    - Created a new CasbinDemo component that displays the user.
    - Added permissions.json file defining access rights for users: alice, bob, and admin.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { CasbinDemo } from './CasbinDemo'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='w-lg mx-auto flex flex-col items-center justify-center h-screen'>
+      <span>1</span>
+      <CasbinDemo user='test_user' />
+    </div>
   )
 }
 
